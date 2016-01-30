07f333599d600e6cb4e3686366055344bf7ca98c
RiflemanSD <sotirisdoudis@windowslive.com>
2016-01-30 21:28:15 +0200

    NEW: When u click at Save Excel... Menu Item will now open a JFileChooser to choose where the file will be save. Added a Test class to test the JFileChooser to work poperly

--- src/org/riflemansd/businessprofit/main/GUIDataTest.java
+++ src/org/riflemansd/businessprofit/main/GUIDataTest.java
@@ -6,6 +6,7 @@
 package org.riflemansd.businessprofit.main;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,12 +14,15 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.Date;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import org.riflemansd.businessprofit.excel.MyExcelDocument;
@@ -32,24 +36,25 @@ import org.riflemansd.jxsortabletable.JXSortableTable;
  * @author sotir
  */
 public class GUIDataTest extends javax.swing.JFrame {
+
     private JXSortableTable table;
     private SearchPanel sPanel;
     private ResaultPanel resaultPanel;
     private int currSelectedRow;
+
     /**
      * Creates new form GUIDataTest
      */
     public GUIDataTest() {
         currSelectedRow = -1;
-        
+
         initComponents();
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        
-        table = new JXSortableTable("ID,Ημερομηνία,Κατηγορία,Έσοδο/Έξοδο,Ποσό(€),Αιτιολογία,Παραδόσεις,Παραλαβές", 0, new Date(),"string","string",1.5,"s",1,1);
-        
+
+        table = new JXSortableTable("ID,Ημερομηνία,Κατηγορία,Έσοδο/Έξοδο,Ποσό(€),Αιτιολογία,Παραδόσεις,Παραλαβές", 0, new Date(), "string", "string", 1.5, "s", 1, 1);
+
         //table.setRowSelectionAllowed(true);
         //table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-
         table.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
@@ -61,105 +66,105 @@ public class GUIDataTest extends javax.swing.JFrame {
                 }
 
                 currSelectedRow = table.getSelectedRow();
-                if (currSelectedRow < 0)
+                if (currSelectedRow < 0) {
                     return;
-                if (e.isPopupTrigger() && e.getComponent() instanceof JTable ) {
+                }
+                if (e.isPopupTrigger() && e.getComponent() instanceof JTable) {
                     JPopupMenu popup = new JPopupMenu();
-                    
+
                     JMenuItem previewItem = new JMenuItem("Preview");
                     previewItem.addActionListener(new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
                             Object[] row = table.getRowAt(currSelectedRow);
                             String[] datas = rowToString(row);
-                            
+
                             PreviewRow pr = new PreviewRow(datas);
-                            
+
                             pr.setLocationRelativeTo(null);
                             pr.setVisible(true);
                         }
                     });
                     popup.add(previewItem);
-                    
+
                     JMenuItem deleteItem = new JMenuItem("Delete");
                     deleteItem.addActionListener(new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
-                            int a = JOptionPane.showConfirmDialog(rootPane, "Πρόκειται να διαγραφοόυν όλα τα δεδομένα της γραμμής\nΕίστε σίγουρος ότι θέλετε να διαγραφούν?", "Διαγραφή Δεδομένων",JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+                            int a = JOptionPane.showConfirmDialog(rootPane, "Πρόκειται να διαγραφοόυν όλα τα δεδομένα της γραμμής\nΕίστε σίγουρος ότι θέλετε να διαγραφούν?", "Διαγραφή Δεδομένων", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
                             if (a == 0) {
                                 Object[] row = table.getRowAt(currSelectedRow);
-                                
+
                                 int tid = -1;
-                                int id = Integer.parseInt((String)row[0]);
+                                int id = Integer.parseInt((String) row[0]);
                                 String cat = (String) row[2];
                                 String inout = (String) row[3];
-                                
+
                                 if (cat.equals("Δέματα") && inout.equals("Έσοδο")) {
                                     tid = 3;
-                                }
-                                else if (inout.equals("Έσοδο")) {
+                                } else if (inout.equals("Έσοδο")) {
                                     tid = 2;
-                                }
-                                else if (inout.equals("Έξοδο")) {
+                                } else if (inout.equals("Έξοδο")) {
                                     tid = 1;
                                 }
-                                
+
                                 BusinessProfit.database.delete(tid, id);
-                                
+
                                 defineData();
                                 JOptionPane.showMessageDialog(rootPane, "Τα δεδομένα της γραμμής διαγράφτηκαν", "Επιτυχία", JOptionPane.INFORMATION_MESSAGE);
-            
+
                             }
                         }
                     });
                     popup.add(deleteItem);
-                    
+
                     popup.show(e.getComponent(), e.getX(), e.getY());
                 }
             }
         });
-        
+
         table.getColumn(0).sizeWidthToFit();
         table.getColumn(3).sizeWidthToFit();
         table.getColumn(4).sizeWidthToFit();
         table.getColumn(6).sizeWidthToFit();
         table.getColumn(7).sizeWidthToFit();
-        
+
         tablePanel.setLayout(new BorderLayout());
         JScrollPane sc = new JScrollPane(table);
         tablePanel.add(sc, BorderLayout.CENTER);
-        
+
         searchPanel.setLayout(new BorderLayout());
         sPanel = new SearchPanel(this);
         searchPanel.add(sPanel, BorderLayout.CENTER);
-        
+
         rPanel.setLayout(new BorderLayout());
         resaultPanel = new ResaultPanel();
         rPanel.add(resaultPanel, BorderLayout.CENTER);
-        
+
         defineData();
-        
+
         this.setLocationRelativeTo(null);
-        this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 
     public String[] rowToString(Object[] objects) {
         String[] datas = new String[8];
-        
-        datas[0] = (String)objects[0];
-        datas[1] = BusinessProfit.database.dateToString((Date)objects[1]);
-        datas[2] = (String)objects[2];
-        datas[3] = (String)objects[3];
+
+        datas[0] = (String) objects[0];
+        datas[1] = BusinessProfit.database.dateToString((Date) objects[1]);
+        datas[2] = (String) objects[2];
+        datas[3] = (String) objects[3];
         datas[4] = "" + objects[4];
-        datas[5] = (String)objects[5];
+        datas[5] = (String) objects[5];
         datas[6] = "" + objects[6];
         datas[7] = "" + objects[7];
-        
+
         return datas;
     }
+
     public Object[] stringToRow(String[] objects) {
         Object[] datas = new Object[8];
-        
+
         datas[0] = Integer.parseInt(objects[0]);
         datas[1] = BusinessProfit.database.getDate(objects[1]);
         datas[2] = objects[2];
@@ -168,137 +173,172 @@ public class GUIDataTest extends javax.swing.JFrame {
         datas[5] = objects[5];
         datas[6] = Integer.parseInt(objects[6]);
         datas[7] = Integer.parseInt(objects[7]);
-        
+
         return datas;
     }
-    
+
     public void defineData() {
         double inValue = 0;
         double outValue = 0;
-        
+
         table.deleteAllRows();
-        
+
         String[] in = BusinessProfit.database.getIn().split("\n");
         String[] out = BusinessProfit.database.getOut().split("\n");
         String[] packIn = BusinessProfit.database.getPackIn().split("\n");
-        
+
         for (String i : in) {
             Object[] data = this.inToTableRow(i);
             if (data != null) {
                 table.addRow(data);
-                inValue += (double)data[4];
+                inValue += (double) data[4];
             }
         }
         for (String p : packIn) {
             Object[] data = this.packInToTableRow(p);
             if (data != null) {
                 table.addRow(data);
-                inValue += (double)data[4];
+                inValue += (double) data[4];
             }
         }
         for (String o : out) {
             Object[] data = this.outToTableRow(o);
             if (data != null) {
                 table.addRow(data);
-                outValue += (double)data[4];
+                outValue += (double) data[4];
             }
         }
-        
+
         resaultPanel.updateProfit(inValue, outValue);
     }
-    
+
     private Object[] inToTableRow(String in) {
         //"ID,Ημερομηνία,Κατηγορία,Έσοδο/Έξοδο,Ποσό(€),Αιτιολογία,Παραδόσεις,Παραλαβές"
-        
+
         String cat = sPanel.getCategory();
         int index = sPanel.getInOut();
-        
+
         String[] ind = in.split(",");
         Object[] objects = new Object[6];
-        
+
         objects[2] = BusinessProfit.database.getCategory(Integer.parseInt(ind[1])).getName();
-        if (!cat.equals("Όλες")) if (!cat.equals((String)objects[2])) return null;
-        if (index != 0) if (index == 2)  return null;
-        
+        if (!cat.equals("Όλες")) {
+            if (!cat.equals((String) objects[2])) {
+                return null;
+            }
+        }
+        if (index != 0) {
+            if (index == 2) {
+                return null;
+            }
+        }
+
         objects[1] = BusinessProfit.database.getDate(ind[4]);
-        if (!checkDate((Date)objects[1])) return null;
-        
+        if (!checkDate((Date) objects[1])) {
+            return null;
+        }
+
         objects[0] = ind[0];
         objects[3] = "Έσοδο";
         objects[4] = Double.parseDouble(ind[2]);
         objects[5] = ind[3];
-        
+
         return objects;
     }
+
     private Object[] outToTableRow(String out) {
         //"ID,Ημερομηνία,Κατηγορία,Έσοδο/Έξοδο,Ποσό(€),Αιτιολογία,Παραδόσεις,Παραλαβές"
         String cat = sPanel.getCategory();
         int index = sPanel.getInOut();
-        
+
         String[] ind = out.split(",");
         Object[] objects = new Object[6];
-        
+
         objects[2] = BusinessProfit.database.getCategory(Integer.parseInt(ind[1])).getName();
-        if (!cat.equals("Όλες")) if (!cat.equals((String)objects[2])) return null;
-        if (index != 0) if (index == 1)  return null;
-        
+        if (!cat.equals("Όλες")) {
+            if (!cat.equals((String) objects[2])) {
+                return null;
+            }
+        }
+        if (index != 0) {
+            if (index == 1) {
+                return null;
+            }
+        }
+
         objects[1] = BusinessProfit.database.getDate(ind[4]);
-        if (!checkDate((Date)objects[1])) return null;
-        
+        if (!checkDate((Date) objects[1])) {
+            return null;
+        }
+
         objects[0] = ind[0];
         objects[3] = "Έξοδο";
         objects[4] = Double.parseDouble(ind[2]);
         objects[5] = ind[3];
-        
+
         return objects;
-        
+
     }
+
     private Object[] packInToTableRow(String packIn) {
         //"ID,Ημερομηνία,Κατηγορία,Έσοδο/Έξοδο,Ποσό(€),Αιτιολογία,Παραδόσεις,Παραλαβές"
         String cat = sPanel.getCategory();
         int index = sPanel.getInOut();
-        
+
         String[] ind = packIn.split(",");
         Object[] objects = new Object[8];
-        
+
         objects[2] = BusinessProfit.database.getCategory(Integer.parseInt(ind[1])).getName();
-        if (!cat.equals("Όλες")) if (!cat.equals((String)objects[2])) return null;
-        if (index != 0) if (index == 2)  return null;
-        
+        if (!cat.equals("Όλες")) {
+            if (!cat.equals((String) objects[2])) {
+                return null;
+            }
+        }
+        if (index != 0) {
+            if (index == 2) {
+                return null;
+            }
+        }
+
         objects[1] = BusinessProfit.database.getDate(ind[6]);
-        
-        if (!checkDate((Date)objects[1])) return null;
-        
+
+        if (!checkDate((Date) objects[1])) {
+            return null;
+        }
+
         objects[0] = ind[0];
         objects[3] = "Έσοδο";
         objects[4] = Double.parseDouble(ind[2]);
         objects[5] = ind[3];
         objects[6] = Integer.parseInt(ind[4]);
         objects[7] = Integer.parseInt(ind[5]);
-        
+
         return objects;
-        
+
     }
-    
+
     private boolean checkDate(Date date) {
         Date[] dates = sPanel.getDates();
-        
+
         if (this.sPanel.isDateSearch()) {
             if (dates[0].compareTo(dates[1]) == 0) {
-                if (dates[0].compareTo(date) != 0) return false;
-            }
-            else {
-               if (dates[0].compareTo(date) != 0 && dates[1].compareTo(date) != 0) {
-                if (!((date).after(dates[0]) && (date).before(dates[1]))) return false;
-               } 
+                if (dates[0].compareTo(date) != 0) {
+                    return false;
+                }
+            } else {
+                if (dates[0].compareTo(date) != 0 && dates[1].compareTo(date) != 0) {
+                    if (!((date).after(dates[0]) && (date).before(dates[1]))) {
+                        return false;
+                    }
+                }
             }
         }
-        
+
         return true;
     }
-    
+
     /**
-     * 
+     *
      * @param o1 - 27-1-2016
      * @param o2 - 28-1-2016
      * @return o2 &gt; o1 = -1, o2 &lt; o1 = 1, o2 == o1 = 0
@@ -314,7 +354,7 @@ public class GUIDataTest extends javax.swing.JFrame {
             return 0;
         }
     }
-    
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -414,35 +454,54 @@ public class GUIDataTest extends javax.swing.JFrame {
     }//GEN-LAST:event_MenuItemExitActionPerformed
 
     private void MenuItemSaveAsExcelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MenuItemSaveAsExcelActionPerformed
+        String fileName = "database";
+
+        JFileChooser fc = new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        //fc.setCurrentDirectory(null);
+
+        int returnVal = fc.showSaveDialog(rootPane);
+        //int returnVal = fc.showOpenDialog(rootPane);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+
+            System.out.println("Opening: " + file.getAbsolutePath());
+            System.out.println("Opening: " + file.getName());
+            
+            if (!file.getAbsolutePath().equals(file.getName())) {
+                fileName = file.getAbsolutePath();
+            }
+        } else {
+            return;
+        }
+        fileName += ".xlsx";
+        System.out.println(fileName);
+
         int numberOfColumns = this.table.getColumnCount();
         int numberOfRows = this.table.getRowCount();
-        String fileName = "test.xlsx";
-        
+
         MyExcelDocument doc = new MyExcelDocument(fileName);
-        
+
         for (int i = 0; i < numberOfColumns; i++) {
             String header = (String) table.getColumn(i).getHeaderValue();
-            
+
             doc.setHeader(0, 0, i, header);
         }
-        
+
         for (int i = 1; i < numberOfRows; i++) {
             Object[] datas = this.table.getRowAt(i);
-            
+
             int j = 0;
             datas[0] = Integer.parseInt((String) datas[0]);
             for (Object o : datas) {
                 if (o instanceof Integer) {
-                    doc.setDouble(0, i, j, (int)o);
-                }
-                else if (o instanceof Number) {
-                    doc.setDouble(0, i, j, (double)o);
-                }
-                else if (o instanceof Date) {
-                    doc.setDate(0, i, j, (Date)o);
-                }
-                else {
-                    doc.setString(0, i, j, (String)o);
+                    doc.setDouble(0, i, j, (int) o);
+                } else if (o instanceof Number) {
+                    doc.setDouble(0, i, j, (double) o);
+                } else if (o instanceof Date) {
+                    doc.setDate(0, i, j, (Date) o);
+                } else {
+                    doc.setString(0, i, j, (String) o);
                 }
                 j++;
             }
@@ -450,14 +509,8 @@ public class GUIDataTest extends javax.swing.JFrame {
         for (int i = 0; i < numberOfRows; i++) {
             doc.autoFillColumhWidth(0, i);
         }
-        Thread t1 = new Thread(new Runnable() {
-            public void run() {
-                JOptionPane.showMessageDialog(rootPane, "Saving...");
-            }
-       });  
-        t1.start();
-        
         doc.save();
+
         OpenProjectFiles.openFile(fileName);
     }//GEN-LAST:event_MenuItemSaveAsExcelActionPerformed
 
@@ -493,7 +546,7 @@ public class GUIDataTest extends javax.swing.JFrame {
             public void run() {
                 GUIDataTest test = new GUIDataTest();
                 test.setVisible(true);
-                
+
                 test.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             }
         });
